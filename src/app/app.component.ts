522a8ef6abbdc069479308aa18e5af5f208fe5c3
Nixon Augustine <nixon_augustine@hotmail.com>
2020-05-14 23:52:27 +0530

    Angular Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'event-binding';
+  name = '';
+  kPress='';
+  kUp='';
+  kDown='';
+  blurText = '';
+  displayName(){
+    this.name = 'Welcome to Angular Training';
+  } 
+
+  displayValue(event){
+    console.log(event.key);
+    this.kPress = this.kPress + event.key;
+    this.kUp = this.kUp + event.key;
+    this.kDown = this.kDown + event.key;
+    if(event.key === 'Enter'){
+      console.log('You have Pressed Enter');
+    }
+    
+  }
+  blurEvent(event){
+    this.blurText = event.target.value;
+  }
+
+  mouseEnterEvent(){
+    alert("Mouse Entered");
+  }
+
+  mouseLeftEvent(){
+    alert("mouse Left");
+  } 
+
 }
